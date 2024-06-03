2f32923b3b505f7b3926ae65fe709cecf073f182
Kamil Mróz <kamilmp17@gmail.com>
2024-06-03 21:21:50 +0200

    modified yaml

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = " django-insecure-jsq4yg4mu00*$d5guc@hi#=_8ecx&&9^&%8$)=!+!s1avjjp_%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = False
 
 NPM_BIN_PATH = env("NPM_PATH")
 
