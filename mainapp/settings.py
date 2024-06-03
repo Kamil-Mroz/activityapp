7b3a7ee74140bca7c531e8aa7333c950538e2876
Kamil Mróz <kamilmp17@gmail.com>
2024-06-03 21:24:21 +0200

    modified yaml

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -210,11 +210,11 @@ LOGGING = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
