501ee79f384185d9ac3c0764874bf75b75c82776
Kamil Mróz <kamilmp17@gmail.com>
2024-06-03 21:39:27 +0200

    deleted theme

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -255,7 +255,6 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "theme", "static"),
 ]
 STATIC_ROOT = "static_root"
 
